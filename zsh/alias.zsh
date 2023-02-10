787ec53dfb3a6687330709d78b716155d71c2e25
Håkon Carson Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-02-10 15:55:17 +0100

    feat: set up npm alias to do nothing

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,3 +4,5 @@ alias lla="exa --all --long --header --git --icons"
 alias print-path='echo -e ${PATH//:/\\n}'
 alias _cat='command cat'
 alias cat=bat
+alias _npm='command npm'
+alias npm="echo Use pnpm or yarn instead \(or _npm if you really need to use npm\)"

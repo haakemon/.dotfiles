f6867a9f4e8040d25fa85d4b51732f5d4df3fb78
Håkon Carson Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-03-20 09:00:38 +0100

    chore: rename alias to open ports to more correct name

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,4 +6,4 @@ alias _cat='command cat'
 alias cat=bat
 alias _npm='command npm'
 alias npm="echo Use pnpm or yarn instead \(or _npm if you really need to use npm\)"
-alias fixip="sudo sysctl -w net.ipv4.ip_unprivileged_port_start=443"
+alias fixports="sudo sysctl -w net.ipv4.ip_unprivileged_port_start=443"

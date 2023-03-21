33c363181987c8f04076cce7ffa5d4d20ac595d9
Håkon Carson Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-03-21 15:00:57 +0100

    feat: add alias to fix time drift on wsl

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,10 +1,13 @@
 
-#alias
+# alias
 alias lla="exa --all --long --header --git --icons"
 alias print-path='echo -e ${PATH//:/\\n}'
 alias _cat='command cat'
 alias cat=bat
 alias _npm='command npm'
 alias npm="echo Use pnpm or yarn instead \(or _npm if you really need to use npm\)"
-alias fixports="sudo sysctl -w net.ipv4.ip_unprivileged_port_start=443"
 alias gitroot="cd $(git rev-parse --show-toplevel)"
+
+# alias with sudo privileges
+alias fixports="sudo sysctl -w net.ipv4.ip_unprivileged_port_start=443"
+alias synctime="sudo ntpdate time.windows.com"

956c61e6597301c8c8cbc0507202d2bf795c3224
Håkon Carson Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-03-21 08:31:20 +0100

    feat: add gitroot alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,3 +7,4 @@ alias cat=bat
 alias _npm='command npm'
 alias npm="echo Use pnpm or yarn instead \(or _npm if you really need to use npm\)"
 alias fixports="sudo sysctl -w net.ipv4.ip_unprivileged_port_start=443"
+alias gitroot="cd $(git rev-parse --show-toplevel)"

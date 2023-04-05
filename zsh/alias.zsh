e0b6c8482fb4a8a16c896da2eef495e302d0841f
Håkon Carson Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-04-05 10:33:38 +0200

    feat: add goto alias for zsh

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,9 +6,31 @@ alias _cat='command cat'
 alias cat=bat
 alias _npm='command npm'
 alias npm="echo Use pnpm or yarn instead \(or _npm if you really need to use npm\)"
-alias gitroot="cd $(git rev-parse --show-toplevel)"
 
 # alias with sudo privileges
 alias openports="sudo sysctl -w net.ipv4.ip_unprivileged_port_start=443"
 alias synctime="sudo ntpdate time.windows.com"
 alias ctop="sudo docker run --rm -ti -v /var/run/docker.sock:/var/run/docker.sock quay.io/vektorlab/ctop:latest"
+
+function goto {
+  typeset -A globalLocations=(
+    gitroot 'Root of git repository'
+    dotfiles "$HOME/.dotfiles"
+    home $HOME
+    code "$HOME/Code"
+    temp "$HOME/temp"
+  )
+
+  if [[ "$1" == 'gitroot' ]]; then
+    local gitroot=$(git rev-parse --show-toplevel)
+    cd "$gitroot" || return
+  elif [[ -n "${globalLocations[$1]}" ]]; then
+    cd "${globalLocations[$1]}" || return
+  else
+    echo "Invalid location parameter '$1'"
+    echo "Valid locations:"
+    for key in "${(@k)globalLocations}"; do
+      printf "%-10s %s\n" "  $key:" "${globalLocations[$key]}"
+    done
+  fi
+}

f93587305247fc50cd41ac4d8c856a918cb1ab47
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-06-02 16:46:59 +0200

    feat: split brew installation so kube and container tools can be installed separately

--- zsh/_setup.sh
+++ zsh/_setup.sh
@@ -8,7 +8,6 @@ if [[ $EUID -eq 0 ]]; then
   exit 1
 fi
 
-
 if [ -e "${HOME}/.zshrc" ]; then
   echo "The file ${HOME}/.zshrc already exists. Remove it before continuing."
   exit 2
@@ -17,8 +16,7 @@ fi
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-echo "source \$HOME/.dotfiles/zsh/.zshrc" > "${HOME}/.zshrc"
-
+echo "source \$HOME/.dotfiles/zsh/.zshrc" >"${HOME}/.zshrc"
 
 function add-zsh-to-shells {
   zsh_shell_path="/home/linuxbrew/.linuxbrew/bin/zsh"
@@ -44,32 +42,61 @@ function add-zsh-to-shells {
 if [[ "$(uname -r)" =~ microsoft ]]; then
   read -p "Enter instance name: " InstanceName
 
-  echo "#!/usr/bin/env zsh" > "${HOME}/.dotfiles/zsh/_wsl-instance-name.zsh"
-  echo WSL_INSTANCE_NAME="${InstanceName//[^a-zA-Z0-9]/_}" >> "${HOME}/.dotfiles/zsh/_wsl-instance-name.zsh"
+  echo "#!/usr/bin/env zsh" >"${HOME}/.dotfiles/zsh/_wsl-instance-name.zsh"
+  echo WSL_INSTANCE_NAME="${InstanceName//[^a-zA-Z0-9]/_}" >>"${HOME}/.dotfiles/zsh/_wsl-instance-name.zsh"
 fi
 
 add-zsh-to-shells
 
+function brewinstall-basics {
+  brew install zsh \
+    fzf \
+    exa \
+    viu \
+    bat \
+    git-delta \
+    grc \
+    fastfetch \
+    romkatv/powerlevel10k/powerlevel10k \
+    bc \
+    gh
+}
+
+function brewinstall-container-tools {
+  brew install podman \
+    podman-compose \
+    lego
+}
+
+function brewinstall-kube-tools {
+  brew install kube-ps1 \
+    kubectx \
+    kubernetes-cli \
+    k9s \
+    lego
+}
+
 brew analytics off
-brew install zsh \
-             fnm \
-             fzf \
-             exa \
-             viu \
-             bat \
-             git-delta \
-             grc \
-             kube-ps1 \
-             kubectx \
-             kubernetes-cli \
-             lego \
-             podman \
-             podman-compose \
-             k9s \
-             bc \
-             gh \
-             fastfetch \
-             romkatv/powerlevel10k/powerlevel10k \
+brewinstall-basics
+
+# Prompt for container tools installation
+read -p "Do you wish to install container tools? [y/N]: " -n 1 -r container_response
+echo # newline
+
+# Execute the script for installing container tools if applicable
+if [[ $container_response =~ ^[Yy]$ ]]; then
+    ./brewinstall-container-tools
+fi
+
+# Prompt for Kubernetes tools installation
+read -p "Do you wish to install Kubernetes tools? [y/N]: " -n 1 -r kube_response
+echo # newline
+
+# Execute the script for installing Kubernetes tools if applicable
+if [[ $kube_response =~ ^[Yy]$ ]]; then
+    ./brewinstall-kube-tools
+fi
+
 
 chsh -s /home/linuxbrew/.linuxbrew/bin/zsh
 

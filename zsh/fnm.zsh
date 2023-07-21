162be01fa1d2069f5c561b347c32e1ec459fc138
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-07-21 13:35:08 +0200

    chore: add fnm path variables last to path

--- zsh/fnm.zsh
+++ zsh/fnm.zsh
@@ -7,5 +7,5 @@ export FNM_MULTISHELL_PATH="${HOME}/.local/state/fnm_multishells/3104_1676014946
 export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
 export FNM_ARCH="x64"
 export FNM_LOGLEVEL="info"
-export PATH="${HOME}/.local/state/fnm_multishells/3104_1676014946641/bin":$FNM_DIR:$PATH
+export PATH="$PATH:${HOME}/.local/state/fnm_multishells/3104_1676014946641/bin":$FNM_DIR
 eval "$(fnm env --use-on-cd)"

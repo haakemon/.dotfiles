b271d940ab4ad6055875ae8462e4362bef74f6d1
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-07-21 13:36:02 +0200

    feat: add explorer alias and comment about how to add code alias

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -2,11 +2,16 @@
 
 # WSL specific utility helpers
 
-ACTUAL_HOST_IP=$(ipconfig.exe | grep IPv4 | head -1 | rev | awk '{print $1}' | rev | tr -d '\r')
+# Since we dont have access to Windows PATH elements (see /etc/wsl.conf interop section), add some tools that is common to use from WSL
+alias explorer="/mnt/c/Windows/explorer.exe"
+
+# VS Code should be started on the Windows side, but install location differs, and is commonly placed in the user folder.
+# This should be added to ~/.zshrc instead, after sourcing ~/.dotfiles/zsh/.zshrc
+# alias code="/mnt/c/Users/{windows-username}/AppData/Local/Programs/Microsoft\ VS\ Code/bin/code"
 
 # Useful to notify that IP of WSL instance has changed, add the expected ip to MACHINE_EXPECTED_IP, and execute check_ip to check
 check_ip() {
-  local MACHINE_ACTUAL_IP=$(hostname -I | xargs | awk '{print $1}') # Pipe to xargs to trim value
+  local MACHINE_ACTUAL_IP=$(curl -s "ifconfig.me/ip")
 
   if [[ -z "${MACHINE_EXPECTED_IP}" ]]; then
     echo "${YELLOW_COLOR}MACHINE_EXPECTED_IP is not set${RESET_COLOR}"

5df886cf8830b0f1d471548ab24239f6a310d20c
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2023-11-28 21:26:17 +0100

    wayland, gpu fixes

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -80,10 +80,6 @@
         ];
       };
     };
-
-  # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-  # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 
   inputs = {

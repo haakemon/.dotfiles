13301ac1434065ec0c0f9f97ca5ac70678376029
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-01-22 13:09:46 +0100

    refactor: move xdg session variables config to home-manager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -159,25 +159,6 @@
     autoUpgrade.allowReboot = true;
   };
 
-  environment.sessionVariables = {
-    XDG_DESKTOP_DIR = "$HOME/Desktop";
-    XDG_DOCUMENTS_DIR = "$HOME/Documents";
-    XDG_DOWNLOAD_DIR = "$HOME/Downloads";
-    XDG_MUSIC_DIR = "$HOME/Music";
-    XDG_PICTURES_DIR = "$HOME/Pictures";
-    XDG_PUBLICSHARE_DIR = "$HOME/Public";
-    XDG_TEMPLATES_DIR = "$HOME/Templates";
-    XDG_VIDEOS_DIR = "$HOME/Videos";
-
-    XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_STATE_HOME = "$HOME/.local/state";
-
-    NUGET_PACKAGES = "$XDG_CACHE_HOME/NuGetPackages";
-    DOTNET_CLI_HOME = "$XDG_CONFIG_HOME/.dotnet";
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- nixos/home.nix
+++ nixos/home.nix
@@ -5,6 +5,25 @@
   home.homeDirectory = "/home/${username}";
   # home.enableDebugInfo = true;
 
+  home.sessionVariables = {
+    XDG_DESKTOP_DIR = "${config.home.homeDirectory}/Desktop";
+    XDG_DOCUMENTS_DIR = "${config.home.homeDirectory}/Documents";
+    XDG_DOWNLOAD_DIR = "${config.home.homeDirectory}/Downloads";
+    XDG_MUSIC_DIR = "${config.home.homeDirectory}/Music";
+    XDG_PICTURES_DIR = "${config.home.homeDirectory}/Pictures";
+    XDG_PUBLICSHARE_DIR = "${config.home.homeDirectory}/Public";
+    XDG_TEMPLATES_DIR = "${config.home.homeDirectory}/Templates";
+    XDG_VIDEOS_DIR = "${config.home.homeDirectory}/Videos";
+
+    XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
+    XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
+    XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
+    XDG_STATE_HOME = "${config.home.homeDirectory}/.local/state";
+
+    NUGET_PACKAGES = "${config.home.sessionVariables.XDG_CACHE_HOME}/NuGetPackages";
+    DOTNET_CLI_HOME = "${config.home.sessionVariables.XDG_CONFIG_HOME}/.dotnet";
+  };
+
   home.packages = with pkgs; [
     # Browsers
     firefox

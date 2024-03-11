445483ed1604cf786c6aa48ab1bdf11a09bee241
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-03-11 15:13:09 +0100

    fix: disable custom sddm theme because its not compatible with qt6

--- nix/os/hosts/common/sddm-theme/default.nix
+++ nix/os/hosts/common/sddm-theme/default.nix
@@ -2,7 +2,7 @@
 { config, pkgs, ... }:
 
 {
-  services.xserver.displayManager.sddm.theme = "sugar-dark";
+  # services.xserver.displayManager.sddm.theme = "sugar-dark"; # not compatible with qt6
 
   environment.systemPackages = let themes = pkgs.callPackage ./sddm-themes.nix { }; in [
     themes.sddm-sugar-dark

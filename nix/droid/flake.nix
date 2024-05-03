763871931db3e91669f67cc97ed896b86cc8c97d
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-05-03 22:29:04 +0000

    initial droid config

---
+++ nix/droid/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Basic example of Nix-on-Droid system config.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+    nix-on-droid = {
+      url = "github:t184256/nix-on-droid/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nix-on-droid }: {
+
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      modules = [ ./nix-on-droid.nix ];
+    };
+
+  };
+}

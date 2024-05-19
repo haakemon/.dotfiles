eb56353faedc7608d7b263880da5f81fd593bcd9
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-05-19 21:59:17 +0200

    fix: remove nameservers from networking

--- nix/os/hosts/common/networking.nix
+++ nix/os/hosts/common/networking.nix
@@ -4,10 +4,6 @@
   networking = {
     hostName = "${config.configOptions.hostname}";
     networkmanager.enable = true;
-    nameservers = [
-      "192.168.2.9"
-      "9.9.9.9"
-    ];
     # hosts = {
     #   "127.0.0.1" = [	"local.dev" ];
     # };

2e99a9970cf1d5c9066ee865075a567a3d69a337
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-05-19 21:57:06 +0200

    feat: add portals

--- nix/os/hosts/common/base.nix
+++ nix/os/hosts/common/base.nix
@@ -65,10 +65,12 @@
     wlr.enable = true;
     xdgOpenUsePortal = true;
     extraPortals = [
+      pkgs.xdg-desktop-portal-kde
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal
     ];
     configPackages = [
+      pkgs.xdg-desktop-portal-kde
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal

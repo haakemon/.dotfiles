e82161074d21042f9965f531ccdbda9ed0f19439
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-07-23 23:18:32 +0200

    chore: change ags build/start

--- ags/entry.js
+++ ags/entry.js
@@ -1,9 +1,8 @@
-const entry = App.configDir + '/main.ts';
-const outdir = App.configDir + '/dist';
+// const entry = `${App.configDir}/main.ts`;
+const outdir = `${App.configDir}/dist`;
 
 async function init() {
   try {
-    await Utils.execAsync(['pnpm', 'run', 'build']);
     await import(`file://${outdir}/main.js`);
   } catch (error) {
     App.quit();
--- ags/start.sh
+++ ags/start.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 
-ags -q
-ags -c ~/.dotfiles/ags/entry.js
+ags --quit
+
+cd ~/.dotfiles/ags
+pnpm run build
+
+ags --config ~/.dotfiles/ags/entry.js
+# ags --inspector --config ~/.dotfiles/ags/entry.js

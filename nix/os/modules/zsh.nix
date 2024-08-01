30dfeb2de09677ada98ae204c47c70146bbf5ef2
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-08-01 23:12:38 +0200

    fix: add new shell aliases for rebuilding

--- nix/os/modules/zsh.nix
+++ nix/os/modules/zsh.nix
@@ -145,7 +145,8 @@
         '';
 
         shellAliases = {
-          nixrebuild = "sudo nixos-rebuild --specialisation 01-niri switch --flake path:${config.configOptions.flake.dir}#${config.configOptions.flake.hash}";
+          nixrebuild-default = "sudo nixos-rebuild switch --flake path:${config.configOptions.flake.dir}#${config.configOptions.flake.hash}";
+          nixrebuild-niri = "sudo nixos-rebuild --specialisation 01-niri switch --flake path:${config.configOptions.flake.dir}#${config.configOptions.flake.hash}";
           nixrebuild-boot = "sudo nixos-rebuild boot --flake path:${config.configOptions.flake.dir}#${config.configOptions.flake.hash}";
           nixflake-update = "sudo nix flake update path:${config.configOptions.flake.dir}";
           gcCleanup = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";

64b1217162141170276815103cafe06d934a68e8
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-08-19 22:38:50 +0200

    chore: remove unused stuff

--- waybar/start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-pkill waybar
-
-waybar -c ~/.dotfiles/waybar/config.jsonc -s ~/.dotfiles/waybar/style.css
-# env GTK_DEBUG=interactive waybar -c ~/.dotfiles/waybar/config.jsonc -s ~/.dotfiles/waybar/style.css

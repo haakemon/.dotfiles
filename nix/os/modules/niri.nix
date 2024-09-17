86cc064694ff20d0b18f681fb85235546d3f9f6c
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-09-17 20:45:32 +0200

    chore: dont use nix config for wezterm

--- nix/os/modules/niri.nix
+++ nix/os/modules/niri.nix
@@ -89,6 +89,7 @@
           { event = "lock"; command = "lock"; }
         ];
         timeouts = [
+          # { timeout = 300; command = "${lib.getExe config.programs.niri.package} msg action power-off-monitors"; }
           { timeout = 300; command = "${pkgs.niri}/bin/niri msg action power-off-monitors"; }
           { timeout = 3600; command = "${pkgs.swaylock-effects}/bin/swaylock -f"; }
           { timeout = 7200; command = "${pkgs.systemd}/bin/systemctl suspend"; }
--- nix/os/modules/wezterm.nix
+++ nix/os/modules/wezterm.nix
@@ -6,39 +6,43 @@
       wezterm = {
         enable = true;
         package = inputs.wezterm.packages.${pkgs.system}.default;
-        extraConfig = ''
-          local wezterm = require('wezterm')
-          local act = wezterm.action
+        # extraConfig = ''
+        #   local wezterm = require('wezterm')
+        #   local act = wezterm.action
 
-          local wezterm = require('wezterm')
-          local act = wezterm.action
+        #   local wezterm = require('wezterm')
+        #   local act = wezterm.action
 
-          local config = {}
-          if wezterm.config_builder then
-            config = wezterm.config_builder()
-          end
+        #   local config = {}
+        #   if wezterm.config_builder then
+        #     config = wezterm.config_builder()
+        #   end
 
-          config.color_scheme = 'Dracula'
-          config.font = wezterm.font 'Victor Mono'
+        #   config.color_scheme = 'Dracula'
+        #   config.font = wezterm.font 'Victor Mono'
 
-          config.scrollback_lines = 5000
-          config.audible_bell = "Disabled"
-          config.enable_scroll_bar = true
-          config.hide_mouse_cursor_when_typing = false
-          config.window_background_opacity = 0.8
-          config.window_close_confirmation = "NeverPrompt"
+        #   config.scrollback_lines = 5000
+        #   config.audible_bell = "Disabled"
+        #   config.enable_scroll_bar = true
+        #   config.hide_mouse_cursor_when_typing = false
+        #   config.window_background_opacity = 0.8
+        #   config.window_close_confirmation = "NeverPrompt"
 
-          config.initial_cols = 156
-          config.initial_rows = 36
+        #   config.initial_cols = 156
+        #   config.initial_rows = 36
 
-          config.keys = {
-            { key = 'Insert', mods = 'CTRL', action = act.CopyTo 'Clipboard' },
-            { key = 'Insert', mods = 'SHIFT', action = act.PasteFrom 'Clipboard' },
-          }
+        #   config.keys = {
+        #     { key = 'Insert', mods = 'CTRL', action = act.CopyTo 'Clipboard' },
+        #     { key = 'Insert', mods = 'SHIFT', action = act.PasteFrom 'Clipboard' },
+        #   }
 
-          return config
-        '';
+        #   return config
+        # '';
       };
+    };
+
+    home.file = {
+      ".wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink "${config.configOptions.userHome}/.dotfiles/wezterm/.wezterm.lua";
     };
   };
 }

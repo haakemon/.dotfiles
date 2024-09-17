4144fee2817f8f420a8353b1da2782e6abf344fc
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-09-17 21:45:48 +0200

    fix: override plasticity version

--- nix/os/modules/3d-printing.nix
+++ nix/os/modules/3d-printing.nix
@@ -14,7 +14,14 @@
           # pkgs.blender
           # pkgs.prusa-slicer
           pkgs.bambu-studio
-          pkgs.plasticity
+          (pkgs.plasticity.overrideAttrs rec {
+            # https://nixpk.gs/pr-tracker.html?pr=340999
+            version = "24.2.0";
+            src = pkgs.fetchurl {
+              url = "https://github.com/nkallen/plasticity/releases/download/v${version}/Plasticity-${version}-1.x86_64.rpm";
+              hash = "sha256-3dHS7chTgoD35AV/q8DHIYl43KbCsoFYEqSQHXm05tg=";
+            };
+          })
           pkgs.fstl
         ];
       };

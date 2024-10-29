413cd1dd6313b3814fc432a1ab8c916b07c2debc
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-10-29 21:07:13 +0100

    refactor: cleanup flake

--- nix/os/flake.nix
+++ nix/os/flake.nix
@@ -3,96 +3,112 @@
 
   outputs =
     { self
-    , nixpkgs
-    , chaotic
-    , home-manager
-    , niri
     , ...
-    } @ inputs:
+    }@inputs:
     let
       odin = import ./hosts/odin/variables-local.nix {
-        lib = nixpkgs.lib;
+        lib = inputs.nixpkgs.lib;
       };
       delling = import ./hosts/delling/variables-local.nix {
-        lib = nixpkgs.lib;
+        lib = inputs.nixpkgs.lib;
       };
       heimdall = import ./hosts/heimdall/variables-local.nix {
-        lib = nixpkgs.lib;
+        lib = inputs.nixpkgs.lib;
+      };
+
+      homeManagerConf = {
+        home-manager = {
+          extraSpecialArgs = {
+            inherit inputs;
+          };
+          backupFileExtension = "backup";
+          useGlobalPkgs = true;
+          useUserPackages = true;
+        };
+      };
+
+      specialArgs = {
+        inherit inputs;
       };
     in
     {
       nixosConfigurations = {
-        odin = nixpkgs.lib.nixosSystem {
+        odin = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
+          specialArgs = specialArgs;
           modules = [
             ./hosts/odin/configuration.nix
-            chaotic.nixosModules.default
-            niri.nixosModules.niri
+            inputs.chaotic.nixosModules.default
+            inputs.niri.nixosModules.niri
             inputs.stylix.nixosModules.stylix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                extraSpecialArgs = { inherit inputs; };
-                backupFileExtension = "backup";
-                useGlobalPkgs = true;
-                useUserPackages = true;
-              };
-            }
+            inputs.home-manager.nixosModules.home-manager
+            homeManagerConf
           ];
         };
 
-        delling = nixpkgs.lib.nixosSystem {
+        delling = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
+          specialArgs = specialArgs;
           modules = [
             ./hosts/delling/configuration.nix
-            chaotic.nixosModules.default
-            niri.nixosModules.niri
+            inputs.chaotic.nixosModules.default
+            inputs.niri.nixosModules.niri
             inputs.stylix.nixosModules.stylix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                extraSpecialArgs = { inherit inputs; };
-                backupFileExtension = "backup";
-                useGlobalPkgs = true;
-                useUserPackages = true;
-              };
-            }
+            inputs.home-manager.nixosModules.home-manager
+            homeManagerConf
           ];
         };
 
-        heimdall = nixpkgs.lib.nixosSystem {
+        heimdall = inputs.nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = { inherit inputs; };
+          specialArgs = specialArgs;
           modules = [
             ./hosts/heimdall/configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                extraSpecialArgs = { inherit inputs; };
-                backupFileExtension = "backup";
-                useGlobalPkgs = true;
-                useUserPackages = true;
-              };
-            }
+            inputs.home-manager.nixosModules.home-manager
+            homeManagerConf
           ];
         };
       };
     };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    niri.url = "github:sodiboo/niri-flake";
-    stylix.url = "github:danth/stylix";
 
-    ags.url = "github:Aylur/ags";
-    wezterm.url = "github:wez/wezterm?dir=nix";
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    chaotic = {
+      url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    niri = {
+      url = "github:sodiboo/niri-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ags = {
+      url = "github:Aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    wezterm = {
+      url = "github:wez/wezterm?dir=nix";
+      # inputs.nixpkgs.follows = "nixpkgs"; # this breaks for some reason
+    };
+
+    zen-browser = {
+      url = "github:MarceColl/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }

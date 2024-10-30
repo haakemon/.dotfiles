7414f8cab00c04f8872d4cf0262451026eba2d4c
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-10-30 06:29:47 +0100

    chore: add options for batsignal

--- nix/os/modules/batsignal.nix
+++ nix/os/modules/batsignal.nix
@@ -9,5 +9,16 @@
     }:
     {
       services.batsignal.enable = true;
+      services.batsignal.extraArgs = [
+        "-d 5" # danger level
+        "-c 10" # critical level
+        "-w 15" # warning level
+
+        "-D ${pkgs.systemd}/bin/systemctl suspend" # danger command
+        "-C Battery at 10%" # critical level message
+        "-W Battery at 15%" # warning level message
+
+        "-P Charging battery"
+      ];
     };
 }

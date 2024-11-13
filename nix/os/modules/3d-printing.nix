c166422dfa4fda915959bb3481e1b6ed50de2870
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-11-13 06:59:34 +0100

    chore: remove redundant override

--- nix/os/modules/3d-printing.nix
+++ nix/os/modules/3d-printing.nix
@@ -14,14 +14,7 @@
           # pkgs.blender
           # pkgs.prusa-slicer
           pkgs.bambu-studio
-          (pkgs.plasticity.overrideAttrs rec {
-            # https://nixpk.gs/pr-tracker.html?pr=340999
-            version = "24.2.0";
-            src = pkgs.fetchurl {
-              url = "https://github.com/nkallen/plasticity/releases/download/v${version}/Plasticity-${version}-1.x86_64.rpm";
-              hash = "sha256-3dHS7chTgoD35AV/q8DHIYl43KbCsoFYEqSQHXm05tg=";
-            };
-          })
+          pkgs.plasticity
           pkgs.fstl
         ];
       };

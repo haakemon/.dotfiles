8c90ee2b5b367bf6009aa5ab9756c5127e847f43
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-11-18 23:02:18 +0100

    chore: fix plasma kdoctor scripts

--- plasma/home/steam-stream.sh
+++ plasma/home/steam-stream.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Steam stream layout
-kscreen-doctor output.DisplayPort-1.rotation.normal \
-               output.DisplayPort-1.mode.1920x1080@60 \
-               output.DisplayPort-0.disable
+kscreen-doctor output.DP-2.rotation.normal \
+               output.DP-2.mode.1920x1080@60 \
+               output.DP-1.disable

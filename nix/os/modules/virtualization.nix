27a0dd7ce9c330a89fe5e91e54e097b2288b072c
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-12-10 11:38:19 +0100

    chore: remove unused pods app

--- nix/os/modules/virtualization.nix
+++ nix/os/modules/virtualization.nix
@@ -1,20 +1,25 @@
-{ config, pkgs, lib, ... }:
+{ config
+, pkgs
+, lib
+, ...
+}:
 
 {
-  environment.systemPackages = [
-    pkgs.podman-compose
-    pkgs.distrobox
-  ] ++ lib.optionals (!config.configOptions.headless) [
-    pkgs.virt-viewer
-    pkgs.spice
-    pkgs.spice-gtk
-    pkgs.spice-protocol
-    pkgs.virtio-win
-    pkgs.win-spice
-    pkgs.libvirt
-    pkgs.libguestfs-with-appliance
-    # pkgs.pods # podman gui
-  ];
+  environment.systemPackages =
+    [
+      pkgs.podman-compose
+      pkgs.distrobox
+    ]
+    ++ lib.optionals (!config.configOptions.headless) [
+      pkgs.virt-viewer
+      pkgs.spice
+      pkgs.spice-gtk
+      pkgs.spice-protocol
+      pkgs.virtio-win
+      pkgs.win-spice
+      pkgs.libvirt
+      pkgs.libguestfs-with-appliance
+    ];
 
   programs = lib.mkIf (!config.configOptions.headless) {
     virt-manager.enable = true;

9202fe695d1c2ed60c61a1711537980318f26621
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2024-12-30 10:20:34 +0100

    chore: rename buildFHSEnv

--- nix/os/modules/base.nix
+++ nix/os/modules/base.nix
@@ -189,7 +189,7 @@
               let
                 base = pkgs.appimageTools.defaultFhsEnvArgs;
               in
-              pkgs.buildFHSUserEnv (
+              pkgs.buildFHSEnv (
                 base
                 // {
                   name = "fhs";

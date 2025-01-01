7319677dee3d310b425d21d0984a62f4c7c500fa
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-01-02 00:24:36 +0100

    fix: ags reattach to monitors

--- ags/app.ts
+++ ags/app.ts
@@ -1,8 +1,37 @@
-import { App } from 'astal/gtk3';
+import { App, Gdk, Gtk } from 'astal/gtk3';
 import style from './style.scss';
 import Bar from './widget/bar/Bar';
 import NotificationPopups from './widget/notification/Popups';
 import AppLauncher from './widget/applauncher/Launcher';
+
+function main() {
+  AppLauncher().hide();
+
+  const bars = new Map<Gdk.Monitor, Gtk.Widget>();
+  const popups = new Map<Gdk.Monitor, Gtk.Widget>();
+
+  // initialize
+  for (const gdkmonitor of App.get_monitors()) {
+    print('initialize', JSON.stringify(gdkmonitor.get_model()));
+    bars.set(gdkmonitor, Bar(gdkmonitor));
+    popups.set(gdkmonitor, NotificationPopups(gdkmonitor));
+  }
+
+  App.connect('monitor-added', (_, gdkmonitor) => {
+    print('monitor-added', JSON.stringify(gdkmonitor.get_model()));
+    bars.set(gdkmonitor, Bar(gdkmonitor));
+    popups.set(gdkmonitor, NotificationPopups(gdkmonitor));
+  });
+
+  App.connect('monitor-removed', (_, gdkmonitor) => {
+    print('monitor-removed', JSON.stringify(gdkmonitor.get_model()));
+    bars.get(gdkmonitor)?.destroy();
+    bars.delete(gdkmonitor);
+
+    popups.get(gdkmonitor)?.destroy();
+    popups.delete(gdkmonitor);
+  });
+}
 
 App.start({
   css: style,
@@ -11,9 +40,5 @@ App.start({
     print(request);
     res('ok');
   },
-  main: () => {
-    App.get_monitors().map(Bar);
-    AppLauncher().hide();
-    App.get_monitors().map(NotificationPopups);
-  },
+  main,
 });

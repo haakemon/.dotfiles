2df51e89b351ea089c09489430b45ba1a68bbd35
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-01-12 23:45:51 +0100

    chore: fix kscreendoctor scripts

--- plasma/home/desktop.sh
+++ plasma/home/desktop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Regular layout
-kscreen-doctor output.DP-2.rotation.left \
-               output.DP-2.mode.2560x1440@60 \
-               output.DP-1.enable \
-               output.DP-1.priority.1
+kscreen-doctor output.DisplayPort-1.rotation.left \
+               output.DisplayPort-1.mode.2560x1440@60 \
+               output.DisplayPort-0.enable \
+               output.DisplayPort-0.priority.1

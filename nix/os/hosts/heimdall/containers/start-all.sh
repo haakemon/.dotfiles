547d0e7499ca903789989ef1bd4fa983e4305a60
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-02-09 20:11:33 +0100

    chore: cleanup docker configs

--- nix/os/hosts/heimdall/containers/start-all.sh
+++ nix/os/hosts/heimdall/containers/start-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BASE_DIR=$(dirname "$(realpath "$0")")
-cd "$BASE_DIR"
+cd "$BASE_DIR" || exit
 for dir in */; do
     abs_path=$(realpath "$dir") # Get the absolute path
     if [ -f "$abs_path/docker-compose.yaml" ] || [ -f "$abs_path/docker-compose.yml" ]; then

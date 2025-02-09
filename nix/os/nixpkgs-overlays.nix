1d25ae4019a8a4f33e796af1f4444ac0d4dcef8a
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-02-09 19:43:37 +0100

    fix: mitm build

--- nix/os/nixpkgs-overlays.nix
+++ nix/os/nixpkgs-overlays.nix
@@ -1,11 +1,22 @@
 inputs: final: prev:
 prev
   // {
+  # https://nixpk.gs/pr-tracker.html?pr=
+
   # https://github.com/NixOS/nixpkgs/issues/309056#issuecomment-2366801752
   vivaldi = prev.vivaldi.overrideAttrs (oldAttrs: {
     dontWrapQtApps = false;
     dontPatchELF = true;
     nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.kdePackages.wrapQtAppsHook ];
   });
+
+  python312Packages = prev.python312Packages // {
+    mitmproxy = prev.python312Packages.mitmproxy.overrideAttrs (oldAttrs: rec {
+      pythonRelaxDeps = oldAttrs.pythonRelaxDeps or [ ] ++ [
+        "pyparsing"
+        "ruamel.yaml"
+      ];
+    });
+  };
 
 }

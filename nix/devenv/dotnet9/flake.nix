8e916c42e630cb976bdf0934e11031121c23f966
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-04-14 10:18:05 +0200

    feat: dotnet 9 devenv

---
+++ nix/devenv/dotnet9/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells."${system}".default = pkgs.mkShell
+        {
+          nativeBuildInputs = with pkgs; [
+            nixpkgs.legacyPackages.${system}.dotnet-sdk_9
+          ];
+
+          shellHook = ''
+            clear
+            export DEVENV=1
+            cd "''${DEVENV_START_DIR:-$HOME}"
+            DEVENV_START_DIR=""
+
+            tput setaf 5
+            printf "Dotnet: "
+            ${nixpkgs.legacyPackages.${system}.dotnet-sdk_9}/bin/dotnet --version
+            tput sgr0
+
+            exec zsh
+          '';
+        };
+    };
+}

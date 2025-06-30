999ef4d21835cfabdcc81b5587f976186899cf20
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-06-30 08:32:16 +0200

    fix: wallpaper

--- nix/os/modules/wallpaper.nix
+++ nix/os/modules/wallpaper.nix
@@ -13,23 +13,39 @@
 
   config = {
     home-manager.users.${config.user-config.name} = {
-      systemd.user.timers."wallpaper" = {
-        Install.WantedBy = [ "timers.target" ];
-        Timer = {
-          OnCalendar = "*:0/30"; # Every 30 minutes
-          Unit = "wallpaper.service";
+      systemd.user = {
+        timers."wallpaper" = {
+          Install.WantedBy = [ "timers.target" ];
+          Timer = {
+            OnCalendar = "*:0/30"; # Every 30 minutes
+            Unit = "wallpaper.service";
+          };
         };
-      };
 
-      systemd.user.services."wallpaper" = {
-        Unit = {
-          Description = "Change wallpaper";
+        services."wallpaper" = {
+          Unit = {
+            Description = "Change wallpaper";
+          };
+          Service = {
+            Type = "exec";
+            ExecStart = config.wallpaper.scriptPath;
+            StandardOutput = "journal";
+            Restart = "no";
+          };
         };
-        Service = {
-          Type = "exec";
-          ExecStart = config.wallpaper.scriptPath;
-          StandardOutput = "journal";
-          Restart = "no";
+
+        services."wallpaper-daemon" = {
+          Unit = {
+            Description = " swww-daemon";
+            After = "graphical-session.target";
+          };
+          Install.WantedBy = [ "graphical-session.target" ];
+          Service = {
+            Type = "exec";
+            ExecStart = "${pkgs.swww}/bin/swww-daemon";
+            StandardOutput = "journal";
+            Restart = "no";
+          };
         };
       };
 
--- waypaper/delling/random.sh
+++ waypaper/delling/random.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 waypaper --random --folder ~/Wallpaper/2560x1440 --monitor eDP-1
-waypaper --random --folder ~/Wallpaper/2560x1440 --monitor DP-8
-waypaper --random --folder ~/Wallpaper/1440x2560 --monitor DP-7
+# waypaper --random --folder ~/Wallpaper/2560x1440 --monitor DP-8
+waypaper --random --folder ~/Wallpaper/5120x1440 --monitor DP-7
+# waypaper --random --folder ~/Wallpaper/1440x2560 --monitor DP-7
+waypaper --random --folder ~/Wallpaper/1440x2560 --monitor DP-3

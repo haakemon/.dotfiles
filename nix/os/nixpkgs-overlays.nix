6b5cb87e8639dc0658fd5eec70b2a02bb7c32c84
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-09-09 08:14:08 +0200

    chore: remove spotify-player overlay because playback issues has been fixed

--- nix/os/nixpkgs-overlays.nix
+++ nix/os/nixpkgs-overlays.nix
@@ -4,6 +4,5 @@ prev
   vivaldi = import ./overlays/vivaldi config inputs final prev;
   firefox = import ./overlays/firefox config inputs final prev;
   zen-browser = import ./overlays/zen-browser config inputs final prev;
-  spotify-player = import ./overlays/spotify-player config inputs final prev;
   pnpm = import ./overlays/pnpm config inputs final prev;
 }

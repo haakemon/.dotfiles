675b6c3095c2112855b7b1d3942dce41dd3ba7ea
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-09-15 13:49:52 +0200

    chore: update pnpm and lockfile

--- nix/os/overlays/pnpm/default.nix
+++ nix/os/overlays/pnpm/default.nix
@@ -1,8 +1,8 @@
 config: inputs: final: prev:
 prev.pnpm.overrideAttrs (oldAttrs: {
-  version = "10.15.1";
+  version = "10.16.1";
   src = prev.fetchurl {
-    url = "https://registry.npmjs.org/pnpm/-/pnpm-10.15.1.tgz";
-    hash = "sha256-jFOvAq4+wfsK51N3+NTWIXwtfL5vA8FjUMq/dJPebv8=";
+    url = "https://registry.npmjs.org/pnpm/-/pnpm-10.16.1.tgz";
+    hash = "sha256-t36Sug1ZpjcrbFBBu7P4ZvuF6SffMzgn8Mf1d8XhpxM=";
   };
 })

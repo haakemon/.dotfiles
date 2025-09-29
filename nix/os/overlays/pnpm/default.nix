cc56971673976063e1dfac928beb5b7c774e924a
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-09-29 10:13:21 +0200

    chore: update pnpm to 10.17.1

--- nix/os/overlays/pnpm/default.nix
+++ nix/os/overlays/pnpm/default.nix
@@ -1,12 +1,12 @@
 config: inputs: final: prev:
 let
-  version = "10.17.0";
+  version = "10.17.1";
 in
 prev.pnpm.overrideAttrs (oldAttrs: {
   inherit version;
   src = prev.fetchurl {
     url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-    hash = "sha256-vZ7FQXZBOR4KyzkSspEr1bA4VAeoLalHRKdAe1Z/208=";
+    hash = "sha256-oeATP2gBwTA5rkEwnl5afRBYo6Hh7dAgpJRKg8U2jQQ=";
     # hash = "";
   };
 })

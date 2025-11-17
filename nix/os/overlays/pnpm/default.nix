b237a44f8c7353a13a9be588dae3c2b49efe13ee
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-11-17 10:52:51 +0100

    chore: update pnpm to 10.22.0

--- nix/os/overlays/pnpm/default.nix
+++ nix/os/overlays/pnpm/default.nix
@@ -1,12 +1,12 @@
 config: inputs: final: prev:
 let
-  version = "10.18.3";
+  version = "10.22.0";
 in
 prev.pnpm.overrideAttrs (oldAttrs: {
   inherit version;
   src = prev.fetchurl {
     url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-    hash = "sha256-eX0AWbxfwzVtecaBYVoTfHs15O/AOkRJqKGr/LzEvcc=";
+    hash = "sha256-BTqEk+jjKKPG1/9csHm+8ocZFSzPgx5GZrXAMyt3u4g=";
     # hash = "";
   };
 })

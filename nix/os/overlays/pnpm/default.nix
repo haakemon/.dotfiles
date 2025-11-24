3c02a769d767330f5115aa5ff6b92c494d5aea9b
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-11-24 09:34:19 +0100

    chore: update lockfile and pnpm to v10.23.0

--- nix/os/overlays/pnpm/default.nix
+++ nix/os/overlays/pnpm/default.nix
@@ -1,12 +1,12 @@
 config: inputs: final: prev:
 let
-  version = "10.22.0";
+  version = "10.23.0";
 in
 prev.pnpm.overrideAttrs (oldAttrs: {
   inherit version;
   src = prev.fetchurl {
     url = "https://registry.npmjs.org/pnpm/-/pnpm-${version}.tgz";
-    hash = "sha256-BTqEk+jjKKPG1/9csHm+8ocZFSzPgx5GZrXAMyt3u4g=";
+    hash = "sha256-oc3XtGg4ap14oIHaBdYEnX5ZjbYqKZ25LfIacGKksYM=";
     # hash = "";
   };
 })

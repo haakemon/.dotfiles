f9a3b35025ca263da3b046f273cab18655c028f8
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2025-12-06 07:57:30 +0100

    fix: remove v4l2loopback to fix build issues, not using it

--- nix/os/hosts/odin/hardware-configuration.nix
+++ nix/os/hosts/odin/hardware-configuration.nix
@@ -32,14 +32,7 @@
     };
     kernelModules = [
       "kvm-amd"
-      "v4l2loopback"
     ];
-    extraModulePackages = [
-      config.boot.kernelPackages.v4l2loopback.out
-    ];
-    extraModprobeConfig = ''
-      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
-    '';
   };
 
   fileSystems = {

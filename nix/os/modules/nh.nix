5e088111f8d7992a855fdc72b97b2742d806ed95
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2026-01-02 23:07:04 +0100

    feat: add nh env variable for home-manager

--- nix/os/modules/nh.nix
+++ nix/os/modules/nh.nix
@@ -1,7 +1,8 @@
 { config, pkgs, ... }:
 {
   environment.sessionVariables = {
-    NH_FLAKE = "path:/home/${config.user-config.name}/.dotfiles/nix/os";
+    NH_OS_FLAKE = "path:/home/${config.user-config.name}/.dotfiles/nix/os";
+    NH_HOME_FLAKE = "path:/home/${config.user-config.name}/.dotfiles/nix/home-manager";
   };
   programs.nh = {
     enable = true;

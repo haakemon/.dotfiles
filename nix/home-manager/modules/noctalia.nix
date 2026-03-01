f4227e745ae6f314cd91af9b5d0aba603fd29227
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2026-03-01 15:39:43 +0100

    chore: disable noctalia config symlink

--- nix/home-manager/modules/noctalia.nix
+++ nix/home-manager/modules/noctalia.nix
@@ -23,14 +23,14 @@
       })
     ];
 
-    file = {
-      ".config/noctalia/colors.json".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/colors.json";
-      ".config/noctalia/gui-settings.json".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/gui-settings.json";
-      ".config/noctalia/settings.json".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/settings.json";
-    };
+    # file = {
+    #   ".config/noctalia/colors.json".source =
+    #     config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/colors.json";
+    #   ".config/noctalia/gui-settings.json".source =
+    #     config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/gui-settings.json";
+    #   ".config/noctalia/settings.json".source =
+    #     config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/settings.json";
+    # };
   };
 
   programs.noctalia-shell = {

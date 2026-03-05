ecb47849122847824f8ae7ed1dd8a4f51f42870d
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2026-03-05 23:12:00 +0100

    fix: noctalia config

--- nix/home-manager/modules/noctalia.nix
+++ nix/home-manager/modules/noctalia.nix
@@ -9,20 +9,11 @@
     inputs.noctalia.homeModules.default
   ];
 
-  home = {
-    packages = [
-      (inputs.quickshell.packages.${pkgs.system}.default.override {
-        withJemalloc = true;
-        withQtSvg = true;
-        withWayland = true;
-        withX11 = false;
-        withPipewire = true;
-        withPam = true;
-        withHyprland = false;
-        withI3 = false;
-      })
-    ];
+  programs.noctalia-shell = {
+    enable = true;
+  };
 
+  home = {
     # file = {
     #   ".config/noctalia/colors.json".source =
     #     config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/colors.json";
@@ -31,10 +22,5 @@
     #   ".config/noctalia/settings.json".source =
     #     config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/settings.json";
     # };
-  };
-
-  programs.noctalia-shell = {
-    enable = true;
-    systemd.enable = true;
   };
 }

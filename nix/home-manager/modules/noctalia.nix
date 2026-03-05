8dc9aee91fd10a67dba5eb3438895d90a3756605
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2026-03-05 23:35:29 +0100

    fix: dont sync noctalia colors

--- nix/home-manager/modules/noctalia.nix
+++ nix/home-manager/modules/noctalia.nix
@@ -15,8 +15,8 @@
 
   home = {
     file = {
-      ".config/noctalia/colors.json".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/colors.json";
+      # ".config/noctalia/colors.json".source =
+      #   config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/colors.json";
       ".config/noctalia/plugins.json".source =
         config.lib.file.mkOutOfStoreSymlink "${config.user-config.home}/.dotfiles/quickshell/noctalia/${hostName}/plugins.json";
       ".config/noctalia/settings.json".source =

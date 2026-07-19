26543d634ea5ed8477322a2457148bc996762b85
Håkon Bogsrud <2082481+haakemon@users.noreply.github.com>
2026-07-20 00:01:49 +0200

    feat: update varlock and add force parameter to easily allow updates

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -1,9 +1,14 @@
 ensure_varlock() {
   # https://varlock.dev
-  export PATH="${XDG_CONFIG_HOME:-~/.config}/varlock/bin:$PATH"
+  export PATH="${XDG_CONFIG_HOME:-$HOME/.config}/varlock/bin:$PATH"
 
-  if ! command -v varlock &>/dev/null; then
-    curl -sSfL https://raw.githubusercontent.com/dmno-dev/varlock/43a02c102a936aa61f816c037d3e694ae350ca58/packages/varlock/install.sh | sh -s
+  local force=false
+  if [[ "$1" == "--force" ]]; then
+    force=true
+  fi
+
+  if [[ "$force" == true ]] || ! command -v varlock &>/dev/null; then
+    curl -sSfL https://raw.githubusercontent.com/dmno-dev/varlock/4e37ef7941c586921cff7cacdfcd90f36ed20943/packages/varlock/install.sh | sh -s
   fi
 }
 
